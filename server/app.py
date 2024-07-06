497d9cf8a4f5736dbb28fa61f544bfaec48376f1
odrumz <odrumz64gmail.com>
2024-07-06 09:43:31 +0300

    chat

--- server/app.py
+++ server/app.py
@@ -16,11 +16,109 @@ db.init_app(app)
 
 @app.route('/messages')
 def messages():
-    return ''
+    
+    messages=[]
+    for message in Message.query.all():
+        message_dict=message.to_dict()
+        messages.append(message_dict)
+
+    response=make_response(
+        messages,
+        200
+    )
+
+    return response
 
 @app.route('/messages/<int:id>')
 def messages_by_id(id):
-    return ''
+    
+    message=Message.query.filter(Message.id==id).first()
+
+    message_dict=message.to_dict()
+
+    response=make_response(
+        message_dict,
+        200
+    )
+
+    return response
+
+@app.route('/messages', methods=['GET', 'POST'])
+def create_message():
+    if request.method == 'GET':
+        messages = []
+        for message in Message.query.all():
+            message_dict = message.to_dict()
+            messages.append(message_dict)
+
+        response = make_response(jsonify(messages), 200)
+        return response
+
+    elif request.method == 'POST':
+        new_message = Message(
+            body=request.json.get("body"),
+            username=request.json.get("username"),
+        )
+
+        db.session.add(new_message)
+        db.session.commit()
+
+        message_dict = new_message.to_dict()
+
+        response = make_response(jsonify(message_dict), 201)
+        return response
+
+
+@app.route('/messages/<int:id>', methods=['GET', 'PATCH', 'DELETE'])
+def update_delete_message(id):
+    message= Message.query.filter(Message.id==id).first()
+
+    if message==None:
+        response_body={
+            "message": "Message not found"
+        }
+
+        response=make_response(response_body, 404)
+
+        return response
+
+    else:
+        if request.method == 'GET':
+            message_dict=message.to_dict()
+
+            response=make_response(
+                message_dict,
+                200
+            )
+
+            return response
+
+        elif request.method == 'PATCH':
+            message.body=request.json.get("body")
+            message.username=request.json.get("username")
+
+            db.session.commit()
+
+            message_dict=message.to_dict()
+
+            response=make_response(
+                message_dict,
+                200
+            )
+
+            return response
+
+        elif request.method == 'DELETE':
+            db.session.delete(message)
+            db.session.commit()
+
+            response_body={
+                "message": "Message deleted successfully"
+            }
+
+            response=make_response(response_body, 200)
+
+            return response
 
 if __name__ == '__main__':
     app.run(port=5555)
